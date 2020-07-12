abf457de704a57cab7a6fc5558193b324fcd0260
nitin-dominic <68175121+nitin-dominic@users.noreply.github.com>
2020-07-12 11:26:11 -0500

    Update Reshaping_images.py

--- Reshaping_images.py
+++ Reshaping_images.py
@@ -1,18 +1,8 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[54]:
-
-
+# 
 from PIL import Image
 import os, sys
 path = "C:/Users/nitin/OneDrive/Desktop/Python_Algorithms/Dataset_Preparation/train/sugar_beet/"
 dirs = os.listdir(path)
-
-
-# In[55]:
-
-
 def resize():
 
     for item in dirs:
@@ -21,6 +11,4 @@ def resize():
             f, e = os.path.splitext(path+item)
             imResize = im.resize((224,224), Image.ANTIALIAS)
             imResize.save(f + ' image_.png', 'png', quality=150) # Change it to .jpg, or any file type you want
-
 resize()
-
